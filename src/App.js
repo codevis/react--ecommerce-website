58900823fc4d82ef1bc607bfb6751ec81aad4129
codevis <visvis2k@gmail.com>
2020-10-15 10:35:34 +0530

    added components and pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import HomePage from './pages/homepage/homepage.component';
+
 function App() {
-  return (<div>
-    <h1>hooo</h1>
-  </div>
-    
+  return (
+    <div>
+      <HomePage />
+    </div>
   );
 }
 
